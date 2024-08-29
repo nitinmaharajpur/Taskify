70eb1df0b8d3d2cc0ec7e9efeea080d95dadf770
Nitin Kumar <nitimaharajpur@gmail.com>
2024-08-29 12:44:27 +0530

    first commit

---
+++ src/app/Components/Sidebar/Sidebar.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react'
+import styled from 'styled-components';
+export const Sidebar = () => {
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+const SidebarStyled=styled.nav`
+
+
+`;
---
+++ src/app/context/globalProvider.js
@@ -0,0 +1,22 @@
+"use client";
+
+import React,{createContext,useState,useContext} from "react";
+import themes from "./themes";
+export const GlobalContext=createContext();
+export const  GlobalUpdateContext=createContext();
+
+export const GlobalProvider=({children})=>{
+    const [selectedTheme,setSelectedTheme]=useState(0);
+    const theme=themes[selectedTheme];
+    return(
+        <GlobalContext.Provider 
+         value={{
+            theme,
+         }}
+        >
+            <GlobalUpdateContext.Provider value={setGlobalState}>
+                {children}
+            </GlobalUpdateContext.Provider>
+        </GlobalContext.Provider>
+    )
+}
